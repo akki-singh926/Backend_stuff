b36ad0d37a4bf498314479f6934f0ce4ae5b0a71
Akash Kumar Singh <akkisingh952380@gmail.com>
2025-09-01 13:45:04 +0530

    after implementation of authentication in person

--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 //define the person schema
+const bcrypt= require('bcrypt');
 
 const personSchema=new mongoose.Schema({
     name:{
@@ -31,9 +32,47 @@ const personSchema=new mongoose.Schema({
     },
     salary:{
         type:Number
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
     }
 
 })
+personSchema.pre('save',async function(next){
+    const person=this;
+    //hash the password only when the record is new
+    if(!person.isModified('password'))return next();
+    try{
+        //hash password generation
+        const salt=await bcrypt.genSalt(10);
+
+        const hashedPassword=await bcrypt.hash(person.password,salt);
+        person.password=hashedPassword;
+
+
+next();
+    }
+    catch(err){
+        return next(err);
+
+    }
+})
+personSchema.methods.comparePassword=async function(candidatePassword){
+    try{
+        const isMatch=await bcrypt.compare(candidatePassword,this.password);
+        return isMatch
+
+    }
+    catch(err){
+        throw err;
+
+    }
+}
 const Person=mongoose.model('Person',personSchema);
 module.exports=Person;
 
--- server.js
+++ server.js
@@ -4,19 +4,35 @@ const express=require('express');
 const app = express()
 const db=require('./db');
 const bodyParser=require('body-parser');
+
+//authentication
+const passport=require('./auth');
+
 app.use(bodyParser.json());
+
 const PORT=process.env.PORT||3000;
+const logRequest=(req,res,next)=>{
+  console.log(`${new Date().toLocaleString()} Request made to: ${req.originalUrl}`);
+  next();
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localauthmiddleware=passport.authenticate('local',{session:false});
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-app.get('/login',(req,res)=>{res.send("welcome to my first server");})
+app.get('/login',function(req,res){res.send("welcome to my first server");})
 
 const personRoutes=require('./Routes/personRoutes');
-app.use('/person',personRoutes);
+app.use('/person',localauthmiddleware,personRoutes);
 
 const menuRoutes=require('./Routes/menuRoutes');
 app.use('/menu',menuRoutes);
+//backend live url=https://backend-stuff-3.onrender.com
+
+//middleware function
 
 
 app.listen(PORT)
